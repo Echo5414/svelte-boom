888010751659e8f8e6d408a282483821090cf766
Echo5414 <sschiener@outlook.de>
2024-12-07 23:33:35 +0100

    Initial commit

---
+++ backend/config/middlewares.ts
@@ -0,0 +1,33 @@
+export default [
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'http:', 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'http:', 'https:'],
+          'media-src': ["'self'", 'data:', 'blob:', 'http:', 'https:'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['http://localhost:5174'], // Add your frontend URL here
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'x-strapi-publish'], // Add the custom header here
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
---
+++ backend/config/plugins.ts
@@ -0,0 +1,9 @@
+module.exports = {
+  upload: {
+    config: {
+      providerOptions: {
+        allowedFormats: ['jpeg', 'png', 'gif', 'svg', 'tiff', 'ico', 'dvu', 'webp'], // Add 'webp' here
+      },
+    },
+  },
+};
